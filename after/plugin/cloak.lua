466bd26adb09fa317f92395759feafffcccc45f6
Cristhian <cristhianjhl.web@gmail.com>
2022-09-24 12:50:35 -0500

    fix: cambiando nombre de la dependencia y pasando configuraciones al setup

--- after/plugin/cloak.lua
+++ after/plugin/cloak.lua
@@ -1,7 +1,22 @@
-local status_ok, cloak = pcall(require, "claok")
+local status_ok, cloak = pcall(require, "cloak")
 
 if not status_ok then
     return
 end
 
-cloak.setup {}
+cloak.setup({
+    enabled = true,
+    cloak_character = "*",
+    highlight_group = "Comment",
+    patters = {
+        {
+            -- Match any file starting with '.env'.
+            -- This can be a table to match multiple file patterns.
+            file_pattern = '.env*',
+            -- Match an equals sign and any character after it.
+            -- This can also be a table of patterns to cloak,
+            -- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
+            cloak_pattern = '=.+'
+        },
+    },
+})
